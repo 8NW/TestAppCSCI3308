1f906ba300fe8c2fb97af3b0a05d1942ec1a420d
8nw <aidenwoodard@me.com>
2021-10-12 18:02:35 -0600

    React app + Node.JS and working on google oauth

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react'
+import GoogleLogin from 'react-google-login'
+
+export class AppComponent extends Component{
+  render() {
+    return(
+      <div>
+
+
+      </div>
+    );
+  }
+}
+
+export default App
